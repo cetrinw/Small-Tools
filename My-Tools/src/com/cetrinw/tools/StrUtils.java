bdf32502dfcd120d6dbe77a34d4ea11931bc1e78
cetrinw <cetrinw@sina.com>
2015-12-24 15:10:54 +0800

    ADD inputStream -> String

--- My-Tools/src/com/cetrinw/tools/StrUtils.java
+++ My-Tools/src/com/cetrinw/tools/StrUtils.java
@@ -1,7 +1,9 @@
 package com.cetrinw.tools;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Blob;
 
 /**
@@ -50,6 +52,29 @@ public class StrUtils {
 		return inputStream;
 	}
 	
+	/**
+	 * inputstream -> String 
+	 * @param in
+	 * @return
+	 */
+	public static String inStream2string(InputStream in) {
+		StringBuffer buffer = new StringBuffer();
+		try {
+			if (in != null) {
+				BufferedReader bf;
+				bf = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+				String line = "";
+				while ((line = bf.readLine()) != null) {
+					buffer.append(line);
+				}
+				in.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			new RuntimeException(e.getMessage());
+		}
+		return buffer.toString();
+	}
 	/**
 	 * copy str
 	 * @param str
