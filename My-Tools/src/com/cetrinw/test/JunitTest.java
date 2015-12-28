c56833fb2c4c50a9eeec95ea69e62df4fcbbe35e
cetrinw <cetrinw@sina.com>
2015-12-28 09:03:01 +0800

    add $

--- My-Tools/src/com/cetrinw/test/JunitTest.java
+++ My-Tools/src/com/cetrinw/test/JunitTest.java
@@ -3,17 +3,19 @@ package com.cetrinw.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.junit.Test;
+
 public class JunitTest {
 
 	@org.junit.Test
 	public void testRegEx() {
 		
 		//xxx.html : xxx.php : xxx.htm ： xxx.shtml
-		String regEx = "\\S+\\.(html|shtml|htm|php)";
-		String content1 = "http://google.com/ind123.php";
+		//$  == String.EndWith
+		String regEx = "\\S+\\.(html|shtml|htm|php)$";
+		String content1 = "http://google.com/ind123.php/12312";
 		String content2 = "http://cetrinw.com/ind223ex.html";
-		String content3 = "http://cetrinw.com/ind23ex.htm";
-		
+		String content3 = "http://cetrinw.com/ind23ex.htm/345";
 		
 		Pattern p_html = Pattern.compile(regEx);
 		Matcher result1 = p_html.matcher(content1);
@@ -29,5 +31,12 @@ public class JunitTest {
 		if(result3.find()){
 			System.out.println(result3.group(0) +":find");
 		}
+	}
+	
+	@Test
+	public void testStrLength(){
+		String str= "id,DATA_NUM,DATA_DT,DATA_SEQ,REL_DATE,WEB_TYPE,KEY_WD,BRAND,INDUSTRY,THEMEKEY_WD,THEME_CX,THEME_RG,PRODUCT,LINK_LOC,PROVINCE,CITY,TITLE,CONTENT,CON_SUM,LINK,RAW_TYPE";
+		
+		System.out.println(str.split(",").length);
 	}
 }
