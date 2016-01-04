8b63ae08acca574962f987acd194ac3bd9c280a1
cetrinw <cetrinw@sina.com>
2016-01-04 11:05:36 +0800

    BigDecimal

--- My-Tools/src/com/cetrinw/test/JunitTest.java
+++ My-Tools/src/com/cetrinw/test/JunitTest.java
@@ -1,5 +1,6 @@
 package com.cetrinw.test;
 
+import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,5 +39,14 @@ public class JunitTest {
 		String str= "id,DATA_NUM,DATA_DT,DATA_SEQ,REL_DATE,WEB_TYPE,KEY_WD,BRAND,INDUSTRY,THEMEKEY_WD,THEME_CX,THEME_RG,PRODUCT,LINK_LOC,PROVINCE,CITY,TITLE,CONTENT,CON_SUM,LINK,RAW_TYPE";
 		
 		System.out.println(str.split(",").length);
+	}
+	
+	@Test
+	public void BigDe(){
+		BigDecimal b1 = new BigDecimal("4.5830396368766036E-7");		
+		BigDecimal b2 = new BigDecimal("0.00000045830396368766036");
+
+		System.out.println("b1:"+b1);
+		System.out.println("b2:"+b2);
 	}
 }
